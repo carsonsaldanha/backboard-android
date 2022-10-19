1c84f87a147df660433b53c40ca157b96a219113
Carson Saldanha <carson.saldanha@gmail.com>
2022-10-18 19:48:27 -0500

    Fixed fragment configuration

--- app/src/main/java/com/jrtc/backboard/MainActivity.kt
+++ app/src/main/java/com/jrtc/backboard/MainActivity.kt
@@ -3,7 +3,8 @@ package com.jrtc.backboard
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jrtc.backboard.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ import com.jrtc.backboard.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +23,10 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 //        val appBarConfiguration = AppBarConfiguration(setOf(
