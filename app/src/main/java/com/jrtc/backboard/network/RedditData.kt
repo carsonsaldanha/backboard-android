01fc8b485cac353c2258f0f95e7c7a7f9d1103b7
Carson Saldanha <carson.saldanha@gmail.com>
2022-11-02 15:41:34 -0500

    Initial work

--- app/src/main/java/com/jrtc/backboard/network/RedditData.kt
+++ app/src/main/java/com/jrtc/backboard/network/RedditData.kt
@@ -6,7 +6,7 @@ import com.squareup.moshi.Json
  * This data class defines [RedditData] which includes the data.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
-data class RedditData (
+data class RedditData(
     val data: Data
 )
 
@@ -14,7 +14,7 @@ data class RedditData (
  * This data class defines [Data] which includes the list of posts.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
-data class Data (
+data class Data(
     @Json(name = "children")
     val posts: List<Post>
 )
@@ -23,7 +23,7 @@ data class Data (
  * This data class defines [Post] which includes the data.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
-data class Post (
+data class Post(
     val data: PostData
 )
 
@@ -31,7 +31,7 @@ data class Post (
  * This data class defines [PostData] which includes the title of a Reddit post and URL.
  * The property names of this data class are used by Moshi to match the names of values in JSON.
  */
-data class PostData (
+data class PostData(
     val title: String,
     val url: String
 )
