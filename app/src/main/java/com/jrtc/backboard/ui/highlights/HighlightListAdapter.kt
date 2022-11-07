b0fc84647cace3054edfa35c24efb34b3fd214f1
Carson Saldanha <carson.saldanha@gmail.com>
2022-11-07 15:17:05 -0600

    Some highlight tab optimizations

--- app/src/main/java/com/jrtc/backboard/ui/highlights/HighlightListAdapter.kt
+++ app/src/main/java/com/jrtc/backboard/ui/highlights/HighlightListAdapter.kt
@@ -1,67 +1,69 @@
 package com.jrtc.backboard.ui.highlights
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.jrtc.backboard.databinding.HighlightsListItemBinding
 import com.jrtc.backboard.network.Post
+import com.squareup.picasso.Picasso
 
 /**
  * This class implements a [RecyclerView] [ListAdapter] which uses Data Binding to present [List]
  * data, including computing diffs between lists.
  */
-class HighlightListAdapter() :
+class HighlightListAdapter(private val clickListener: HighlightListener) :
     ListAdapter<Post, HighlightListAdapter.HighlightViewHolder>(DiffCallback) {
 
     class HighlightViewHolder(var binding: HighlightsListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        @SuppressLint("SetJavaScriptEnabled")
-        fun bind(highlight: Post) {
-            binding.highlight = highlight.data
+        fun bind(clickListener: HighlightListener, highlight: Post) {
+            binding.highlight = highlight
+            binding.clickListener = clickListener
             binding.executePendingBindings()
 
-            val highlightVideoURL = highlight.data.url
-            val streamableVideoId = highlightVideoURL.subSequence(23, highlightVideoURL.length)
-            val videoHTML = "<html>\n" +
-                    "  <head>\n" +
-                    "    <style type=\"text/css\">\n" +
-                    "      html,\n" +
-                    "      body {\n" +
-                    "        height: 100%;\n" +
-                    "        width: 100%;\n" +
-                    "        margin: 0;\n" +
-                    "        padding: 0;\n" +
-                    "      }\n" +
-                    "\n" +
-                    "      .frame {\n" +
-                    "        position: relative;\n" +
-                    "        width: 100%;\n" +
-                    "        height: 0;\n" +
-                    "        padding-bottom: 56.25%;\n" +
-                    "      }\n" +
-                    "\n" +
-                    "      iframe {\n" +
-                    "        position: absolute;\n" +
-                    "        top: 0;\n" +
-                    "        left: 0;\n" +
-                    "        overflow: hidden;\n" +
-                    "      }\n" +
-                    "    </style>\n" +
-                    "  </head>\n" +
-                    "  <body>\n" +
-                    "    <div class='frame'>\n" +
-                    "      <iframe src=\"https://streamable.com/e/$streamableVideoId\" width='100%' height='100%' allowfullscreen frameborder='0' scrolling='no'></iframe>\n" +
-                    "    </div>\n" +
-                    "  </body>\n" +
-                    "</html>"
-            binding.highlightWebView.settings.javaScriptEnabled = true
-            binding.highlightWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
-            binding.highlightWebView.loadData(videoHTML, "text/html", null)
+            Picasso.get().load(highlight.data.media?.embed?.thumbnailUrl).into(binding.highlightThumbnailImageView)
+
+
+//            val highlightVideoURL = highlight.data.url
+//            val streamableVideoId = highlightVideoURL.subSequence(23, highlightVideoURL.length)
+//            val videoHTML = "<html>\n" +
+//                    "  <head>\n" +
+//                    "    <style type=\"text/css\">\n" +
+//                    "      html,\n" +
+//                    "      body {\n" +
+//                    "        height: 100%;\n" +
+//                    "        width: 100%;\n" +
+//                    "        margin: 0;\n" +
+//                    "        padding: 0;\n" +
+//                    "      }\n" +
+//                    "\n" +
+//                    "      .frame {\n" +
+//                    "        position: relative;\n" +
+//                    "        width: 100%;\n" +
+//                    "        height: 0;\n" +
+//                    "        padding-bottom: 56.25%;\n" +
+//                    "      }\n" +
+//                    "\n" +
+//                    "      iframe {\n" +
+//                    "        position: absolute;\n" +
+//                    "        top: 0;\n" +
+//                    "        left: 0;\n" +
+//                    "        overflow: hidden;\n" +
+//                    "      }\n" +
+//                    "    </style>\n" +
+//                    "  </head>\n" +
+//                    "  <body>\n" +
+//                    "    <div class='frame'>\n" +
+//                    "      <iframe src=\"https://streamable.com/e/$streamableVideoId\" width='100%' height='100%' allowfullscreen frameborder='0' scrolling='no'></iframe>\n" +
+//                    "    </div>\n" +
+//                    "  </body>\n" +
+//                    "</html>"
+//            binding.highlightWebView.settings.javaScriptEnabled = true
+//            binding.highlightWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+//            binding.highlightWebView.loadData(videoHTML, "text/html", null)
         }
 
     }
@@ -83,7 +85,11 @@ class HighlightListAdapter() :
 
     override fun onBindViewHolder(holder: HighlightViewHolder, position: Int) {
         val highlight = getItem(position)
-        holder.bind(highlight)
+        holder.bind(clickListener, highlight)
     }
 
+}
+
+class HighlightListener(val clickListener: (highlight: Post) -> Unit) {
+    fun onClick(highlight: Post) = clickListener(highlight)
 }
--- app/src/main/java/com/jrtc/backboard/ui/highlights/HighlightListFragment.kt
+++ app/src/main/java/com/jrtc/backboard/ui/highlights/HighlightListFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.jrtc.backboard.R
 import com.jrtc.backboard.databinding.FragmentHighlightsBinding
 
 class HighlightListFragment : Fragment() {
@@ -23,7 +25,10 @@ class HighlightListFragment : Fragment() {
 
         // Calls the view model method that calls the Reddit api
         viewModel.getHighlightsList()
-        binding.highlightsRecyclerView.adapter = HighlightListAdapter()
+        binding.highlightsRecyclerView.adapter = HighlightListAdapter(HighlightListener { highlight ->
+            viewModel.onHighlightClicked(highlight)
+            findNavController().navigate(R.id.action_navigation_highlights_to_navigation_video)
+        })
 
         // Inflates the layout for this fragment
         return binding.root
